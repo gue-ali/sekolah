2f9398a84e17412f17762bcb821348111b873062
gue-ali <55520049+gue-ali@users.noreply.github.com>
2021-10-25 16:34:33 +0700

    jak
    yaaa

--- akademik/models/absensi.py
+++ akademik/models/absensi.py
@@ -7,7 +7,7 @@ class absensi(models.Model):
     _rec_name       = 'user_guru'
     
     name            = fields.Datetime(string='Waktu', required=True, readonly=False, select=True, default=lambda self: fields.datetime.now())
-    user_guru       = fields.Many2one(comodel_name='hr.employee', string='Nama Guru', store=True)
+    user_guru       = fields.Many2one(comodel_name='hr.employee', string='Nama Guru')
     ruang_kelas     = fields.Many2one(comodel_name='cdn.ruang_kelas', string='Ruang kelas',  required=True)
     mata_pelajaran  = fields.Selection(string='Status Pernikahan', selection=[('single', 'Belum Kawin'), ('married', 'Menikah'),('divorced', 'Cerai Hidup'),('cerai', 'Cerai Mati'),], related='user_guru.marital')
     absen_ids       = fields.One2many(comodel_name='akademik.kehadiran', inverse_name='absen_id', string='Absen')
@@ -20,8 +20,7 @@ class absensi(models.Model):
                 #self.absensi_lines = [(5,0,0)]
                 for x in self.ruang_kelas.siswa_ids:
                     val = {
-                        'name': x.id,
-                        'absen' : 'hadir'
+                        'name': x.id
                     }
                     anggota.append((0,0,val))
                 rec.absen_ids = anggota
@@ -31,8 +30,9 @@ class kehadiran(models.Model):
     _name           = 'akademik.kehadiran'
     _description    = 'Kehadiran'
 
-    absen_id        = fields.Many2one('akademik.absensi', string='Absensi', required=True, ondelete='cascade')
+    absen_id        = fields.Many2one('akademik.absensi', string='Nama Guru', required=True, ondelete='cascade')
+    tanggal            = fields.Datetime(string='Waktu', related='absen_id.name')
     name            = fields.Many2one(comodel_name='cdn.siswa', string='Siswa')
-    nis             = fields.Char( string="No Induk Siswa", required=True,  help="", related='name.nis')
-    absen           = fields.Selection(string='Kehadiran', selection=[('hadir', 'Hadir'), ('sakit', 'Sakit'),])    
+    nis             = fields.Char(string="No Induk Siswa", required=True,  help="", related='name.nis')
+    absen           = fields.Selection(string='Kehadiran', selection=[('hadir', 'Hadir'), ('sakit', 'Sakit'),  ('ijin', 'Ijin'),  ('alpa', 'Alpa'),])    
     keterangan      = fields.Text(string='Keterangan')
--- akademik/models/mapel.py
+++ akademik/models/mapel.py
@@ -6,13 +6,13 @@ class mapel(models.Model):
     _name               = "akademik.mapel"
     _description        = "Tabel Mata Pelajaran"
 
-    name                = fields.Many2one(comodel_name="cdn.master_kelas",  string="Mata pelajaran", required=True, copy=False, help="")
+
+    name                = fields.Text(string='Mata Pelajaran', required=True)
+    pengajar_id         = fields.Many2one(comodel_name="cdn.guru",  string="Pengajar",  help="", required=True)
     tahunajaran_id      = fields.Many2one(comodel_name="cdn.ref_tahunajaran",  string="Tahun Pelajaran", required=True, help="")
-    pengajar_id         = fields.Many2one(comodel_name="cdn.guru",  string="Pengajar",  help="")
-    jenjang             = fields.Selection(selection=[('sd','SD/MI'),('smp','SMP/MTS'),('sma','SMA/MA')],  string="Jenjang", related='name.jenjang', help="")
-    tingkat             = fields.Selection(selection=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'), ('11', '11'), ('12', '12')],  string="Kelas", related='name.tingkat', help="")
-    jurusan_id          = fields.Many2one(comodel_name='cdn.master_jurusan', string='Jurusan / Peminatan', related='name.jurusan_id')
-       
+    jenjang             = fields.Selection(selection=[('sd','SD/MI'),('smp','SMP/MTS'),('sma','SMA/MA')],  string="Jenjang", help="", required=True)
+    tingkat             = fields.Selection(selection=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'), ('11', '11'), ('12', '12')],  string="Kelas", help="", required=True)
+    jurusan_id          = fields.Many2one(comodel_name='cdn.master_jurusan', string='Jurusan / Peminatan', required=True)
 
     keterangan          = fields.Char( string="Keterangan",  help="")
 
